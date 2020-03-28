4bfac54397ad1e2d8d559dc5b9cef8af76ab057e
David Philpott <david.philpott@sainsburys.co.uk>
2020-03-28 22:13:14 +0000

    Complete basic test for assembleAbsolutePath

--- tests/test_foodDailyUpdate.py
+++ tests/test_foodDailyUpdate.py
@@ -1,6 +1,10 @@
+import mock
+
 from foodDailyUpdate import assembleAbsolutePath
 
 
 class TestAssembleAbsolutePath:
-    def test_pathGeneratesCorrectly(self):
-        assert True is False
+    @mock.patch('os.path.expanduser')
+    def test_pathGeneratesCorrectly(self, mocked_expanduser):
+        mocked_expanduser.return_value = "/TestHome"
+        assert assembleAbsolutePath("Test.py") == "/TestHome/Test.py"
