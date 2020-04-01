882c95e10a1536091d5bd3abb0e105357b33872e
David Philpott <david.philpott@sainsburys.co.uk>
2020-04-01 23:36:19 +0100

    Add get cell value, get spreadsheet values, update cell value functions and integrate with main script

--- foodDailyUpdate.py
+++ foodDailyUpdate.py
@@ -4,33 +4,47 @@ from GoogleSheetConnection import GoogleSheetConnection
 from GoogleWorksheetSession import GoogleWorksheetSession
 
 
-def update_cell_value(google_sheet, cell_index, value):
-    google_sheet.update_cell_value(cell_index, value)
+def update_cell_value(state, spreadsheet_name, worksheet_name, cell_index, value):
+    worksheet = state.get(f"worksheet_{spreadsheet_name}{worksheet_name}")
+    worksheet.update_cell_value(cell_index, value)
     return
+
+
+def open_google_worksheet(state, spreadsheet_name: str, worksheet_name: str):
+    worksheet = GoogleWorksheetSession(state, GoogleSheetConnection(state), spreadsheet_name, worksheet_name)
+    state.set({f"worksheet_{spreadsheet_name}{worksheet_name}": worksheet})
+    return
+
+
+def get_sheet_cell_value(state, spreadsheet_name, worksheet_name, index):
+    worksheet = state.get(f"worksheet_{spreadsheet_name}{worksheet_name}")
+    return worksheet.get_cell_value(index)
+
+
+def get_all_sheet_values(state, spreadsheet_name, worksheet_name):
+    worksheet = state.get(f"worksheet_{spreadsheet_name}{worksheet_name}")
+    return worksheet.get_all_sheet_values()
 
 
 def main(state=None):
     if state is None:
         state = State()
 
-    state.info("Requesting client from google using auth")
-    gsheet_connection = GoogleSheetConnection(state)
-
     state.info("Opening food daily and core worksheets")
-    food_daily_info = GoogleWorksheetSession(state, gsheet_connection, "FoodDaily", "Info")
-    food_daily_auto = GoogleWorksheetSession(state, gsheet_connection, "FoodDaily", "Auto")
-    food_daily_manual = GoogleWorksheetSession(state, gsheet_connection, "FoodDaily", "Manual")
-    food_core_history = GoogleWorksheetSession(state, gsheet_connection, "FoodCore", "Historical Food Tracker")
+    open_google_worksheet(state, "FoodDaily", "Info")
+    open_google_worksheet(state, "FoodDaily", "Auto")
+    open_google_worksheet(state, "FoodDaily", "Manual")
+    open_google_worksheet(state, "FoodCore", "Historical Food Tracker")
 
     state.info("Taking current date")
-    current_date = food_daily_info.get_cell_value('C2')
+    current_date = get_sheet_cell_value(state, "FoodDaily", "Info", "C2")
 
     state.info("Reading 'Auto' sheet")
-    df_auto_daily = food_daily_auto.get_all_sheet_values()
+    food_daily_auto_values = get_all_sheet_values(state, "FoodDaily", "Auto")
     state.info("Reading 'Manual' sheet")
-    df_manual_daily = food_daily_manual.get_all_sheet_values()
+    food_daily_manual_values = get_all_sheet_values(state, "FoodDaily", "Manual")
     state.info("Reading 'Historical Tracker' sheet")
-    df_food_history = food_core_history.get_all_sheet_values()
+    food_history_values = get_all_sheet_values(state, "FoodCore", "Historical Food Tracker")
 
     state.info("Initialising transfer lists")
     transfer_date = []
@@ -40,63 +54,63 @@ def main(state=None):
     transfer_protein = []
     transfer_veg = []
     state.info("Appending auto items")
-    if len(df_auto_daily) > 1:
-        for i in range(1, len(df_auto_daily)):
+    if len(food_daily_auto_values) > 1:
+        for i in range(1, len(food_daily_auto_values)):
             transfer_date.append(current_date)
-            transfer_item.append(df_auto_daily[i][0])
-            transfer_quantity.append(df_auto_daily[i][1])
+            transfer_item.append(food_daily_auto_values[i][0])
+            transfer_quantity.append(food_daily_auto_values[i][1])
             transfer_calorie.append("")
             transfer_protein.append("")
             transfer_veg.append("")
 
     state.info("Appending manual items")
-    if len(df_manual_daily) > 1:
-        for i in range(1, len(df_manual_daily)):
+    if len(food_daily_manual_values) > 1:
+        for i in range(1, len(food_daily_manual_values)):
             transfer_date.append(current_date)
-            transfer_item.append(df_manual_daily[i][0])
-            transfer_quantity.append(df_manual_daily[i][1])
-            transfer_calorie.append(df_manual_daily[i][2])
-            transfer_protein.append(df_manual_daily[i][3])
-            transfer_veg.append(df_manual_daily[i][4])
-    start_row = len(df_food_history) + 1
+            transfer_item.append(food_daily_manual_values[i][0])
+            transfer_quantity.append(food_daily_manual_values[i][1])
+            transfer_calorie.append(food_daily_manual_values[i][2])
+            transfer_protein.append(food_daily_manual_values[i][3])
+            transfer_veg.append(food_daily_manual_values[i][4])
+    start_row = len(food_history_values) + 1
     if len(transfer_date) > 0:
         state.info("Sleeping for 101 seconds")
         time.sleep(101)
         state.info("transferring lists to historical tracker")
         for i in range(0, len(transfer_date)):
             row = str(start_row + i)
-            update_cell_value(google_sheet=food_core_history, cell_index='A%s' % row, value=transfer_date[i])
-            update_cell_value(google_sheet=food_core_history, cell_index='B%s' % row, value=transfer_item[i])
-            update_cell_value(google_sheet=food_core_history, cell_index='C%s' % row, value=transfer_quantity[i])
-            update_cell_value(google_sheet=food_core_history, cell_index='D%s' % row, value=transfer_calorie[i])
-            update_cell_value(google_sheet=food_core_history, cell_index='E%s' % row, value=transfer_protein[i])
-            update_cell_value(google_sheet=food_core_history, cell_index='F%s' % row, value=transfer_veg[i])
+            update_cell_value(state, "FoodCore", "Historical Food Tracker", 'A%s' % row, transfer_date[i])
+            update_cell_value(state, "FoodCore", "Historical Food Tracker", 'B%s' % row, transfer_item[i])
+            update_cell_value(state, "FoodCore", "Historical Food Tracker", 'C%s' % row, transfer_quantity[i])
+            update_cell_value(state, "FoodCore", "Historical Food Tracker", 'D%s' % row, transfer_calorie[i])
+            update_cell_value(state, "FoodCore", "Historical Food Tracker", 'E%s' % row, transfer_protein[i])
+            update_cell_value(state, "FoodCore", "Historical Food Tracker", 'F%s' % row, transfer_veg[i])
         state.info("finished transferring items")
 
     else:
         state.info("Nothing to transfer")
-    if len(df_auto_daily) > 1:
+    if len(food_daily_auto_values) > 1:
         state.info("Sleeping for 101 seconds")
         time.sleep(101)
         state.info("Blanking auto items")
-        for i in range(1, len(df_auto_daily)):
+        for i in range(1, len(food_daily_auto_values)):
             row = str(i + 1)
-            update_cell_value(google_sheet=food_daily_auto, cell_index='A%s' % row, value="")
-            update_cell_value(google_sheet=food_daily_auto, cell_index='B%s' % row, value="")
+            update_cell_value(state, "FoodDaily", "Auto", 'A%s' % row, "")
+            update_cell_value(state, "FoodDaily", "Auto", 'B%s' % row, "")
         state.info("finished")
     else:
         state.info("No auto items to blank")
-    if len(df_manual_daily) > 1:
+    if len(food_daily_manual_values) > 1:
         state.info("Sleeping for 101 seconds")
         time.sleep(101)
         state.info("Blanking manual items")
-        for i in range(1, len(df_manual_daily)):
+        for i in range(1, len(food_daily_manual_values)):
             row = str(i+1)
-            update_cell_value(google_sheet=food_daily_manual, cell_index='A%s' % row, value="")
-            update_cell_value(google_sheet=food_daily_manual, cell_index='B%s' % row, value="")
-            update_cell_value(google_sheet=food_daily_manual, cell_index='C%s' % row, value="")
-            update_cell_value(google_sheet=food_daily_manual, cell_index='D%s' % row, value="")
-            update_cell_value(google_sheet=food_daily_manual, cell_index='E%s' % row, value="")
+            update_cell_value(state, "FoodDaily", "Manual", 'A%s' % row, "")
+            update_cell_value(state, "FoodDaily", "Manual", 'B%s' % row, "")
+            update_cell_value(state, "FoodDaily", "Manual", 'C%s' % row, "")
+            update_cell_value(state, "FoodDaily", "Manual", 'D%s' % row, "")
+            update_cell_value(state, "FoodDaily", "Manual", 'E%s' % row, "")
         state.info("finished")
     else:
         state.info("no manual items to blank")
--- tests/test_foodDailyUpdate.py
+++ tests/test_foodDailyUpdate.py
@@ -1,6 +1,10 @@
 import mock
 
 from foodDailyUpdate import main
+from foodDailyUpdate import open_google_worksheet
+from foodDailyUpdate import get_sheet_cell_value
+from foodDailyUpdate import get_all_sheet_values
+from foodDailyUpdate import update_cell_value
 
 
 class TestMain:
@@ -14,3 +18,39 @@ class TestMain:
         monkeypatch.setenv("google_auth_path", "/home/david/projects/food-tracker-update/GoogleAuth.json")
         main(test_state)
         assert True
+
+
+class TestOpenGoogleWorksheet:
+    def test_open_worksheet_is_on_state(self, test_state):
+        open_google_worksheet(test_state, "IntegrationTest", "Sheet1")
+        test_session = test_state.get("worksheet_IntegrationTestSheet1")
+        assert test_session is not None
+
+
+class TestGetSheetCellValue:
+    def test_able_to_retrieve_value(self, test_state):
+        open_google_worksheet(test_state, "IntegrationTest", "Sheet1")
+        test_value = get_sheet_cell_value(test_state, "IntegrationTest", "Sheet1", "A1")
+        assert test_value == "A"
+
+
+class TestGetAllSheetValues:
+    def test_able_to_get_all_values(self, test_state):
+        open_google_worksheet(test_state, "IntegrationTest", "Sheet1")
+        values = get_all_sheet_values(test_state, "IntegrationTest", "Sheet1")
+        expected_output = [["A", "B", "C", "Test Value"],
+                           ["1", "4", "7", "Test Value 1"],
+                           ["2", "5", "8", "Test Value 2"],
+                           ["3", "6", "9", "Test Value 3"]]
+        assert values == expected_output
+
+
+class TestUpdateCellValue:
+    def test_cell_value_updates_correctly(self, test_state):
+        open_google_worksheet(test_state, "IntegrationTest", "PutTest")
+        update_cell_value(test_state, "IntegrationTest", "PutTest", "A1", "")
+        assert get_sheet_cell_value(test_state, "IntegrationTest", "PutTest", "A1") == ""
+        update_cell_value(test_state, "IntegrationTest", "PutTest", "A1", "Test Data")
+        assert get_sheet_cell_value(test_state, "IntegrationTest", "PutTest", "A1") == "Test Data"
+        update_cell_value(test_state, "IntegrationTest", "PutTest", "A1", "")
+        assert get_sheet_cell_value(test_state, "IntegrationTest", "PutTest", "A1") == ""
