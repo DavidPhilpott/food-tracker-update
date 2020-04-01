d068652664500983b846e664c1124854ff906451
David Philpott <david.philpott@sainsburys.co.uk>
2020-04-01 01:56:20 +0100

    Add get all values method to worksheet session

--- tests/test_GoogleWorksheetSession.py
+++ tests/test_GoogleWorksheetSession.py
@@ -14,3 +14,13 @@ class TestReadCellValue:
     def test_read_cell_value_from_sheet(self, test_worksheet_session):
         value = test_worksheet_session.get_cell_value("D1")
         assert value == 'Test Value'
+
+
+class TestGetAllSheetValues:
+    def test_read_all_values_from_sheet(self, test_worksheet_session):
+        values = test_worksheet_session.get_all_sheet_values()
+        expected_output = [["A", "B", "C", "Test Value"],
+                           ["1", "4", "7", "Test Value 1"],
+                           ["2", "5", "8", "Test Value 2"],
+                           ["3", "6", "9", "Test Value 3"]]
+        assert values == expected_output
