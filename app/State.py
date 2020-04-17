8689a0b8f2950774c051bff6523b089266d1f84c
David Philpott <david.philpott@sainsburys.co.uk>
2020-04-17 20:58:33 +0100

    Integrate logging provider with state

--- app/State.py
+++ app/State.py
@@ -24,24 +24,25 @@ class State:
     def get(self, key: str) -> str:
         if not isinstance(key, str):
             raise TypeError(f"Variables must be requested as a string. Requested type is {type(key)}.")
-        self.info(__name__, f"Getting state value for {key}")
+        self.debug(__name__, f"Fetching state value for {key}...")
         if key in self._state.keys():
             key_value = self._state[key]
         else:
-            self.info(__name__, f'{key} not found in local state. Searching OS Env.')
             try:
+                self.debug(__name__, f'{key} not found in local state. Searching OS env...')
                 key_value = self._env_var_provider.get_var(key)
             except KeyError as exc_info:
+                self.warning(__name__, "Cannot find {key} on local state or OS env.")
                 raise
         if type(key_value) == str:
             if key_value.startswith("secret_secure"):
-                self.info(__name__, f'{key} maps to {key_value}, so fetching from SSM as a secure string.')
+                self.debug(__name__, f'{key} maps to {key_value}, so fetching from SSM as a secure string...')
                 key_value = self._aws_parameter_store_provider.get_secure_string(variable_name=key_value)
             elif key_value.startswith("secret_pem"):
-                self.info(__name__, f'{key} maps to {key_value}, so fetching from SSM as a PEM key.')
+                self.debug(__name__, f'{key} maps to {key_value}, so fetching from SSM as a PEM key...')
                 key_value = self._aws_parameter_store_provider.get_secure_pem_key(variable_name=key_value)
             elif key_value.startswith("secret"):
-                self.info(__name__, f'{key} maps to {key_value}, so fetching from SSM as a regular string.')
+                self.debug(__name__, f'{key} maps to {key_value}, so fetching from SSM as a regular string...')
                 key_value = self._aws_parameter_store_provider.get_non_secure_string(variable_name=key_value)
         return key_value
 
@@ -50,7 +51,7 @@ class State:
             raise TypeError(f"Variables must be set as a single dict key-pair. Requested type is {type(key_pair)}.")
         if len(key_pair.keys()) != 1:
             raise ValueError(f"Key-pair submitted must be length 1. Current key-pair dict is length {len(key_pair.keys())}.")
-        self.info(__name__, f"Setting state value for {list(key_pair.keys())[0]}")
+        self.debug(__name__, f"Setting state value for {list(key_pair.keys())[0]}...")
         self._state.update(key_pair)
 
     def info(self, name, message):
