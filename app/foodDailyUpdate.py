aee1a6855eba237b04f63ddf4e37225295137bea
David Philpott <david.philpott@sainsburys.co.uk>
2020-04-19 03:38:24 +0100

    Remove unneeded functions from main script

--- app/foodDailyUpdate.py
+++ app/foodDailyUpdate.py
@@ -1,7 +1,5 @@
 import time
 from app.State import State
-from app.Sessions.GoogleSheetConnection import GoogleSheetConnection
-from app.Sessions.GoogleWorksheetSession import GoogleWorksheetSession
 from app.Actions.OpenGoogleWorksheetSession import open_google_worksheet_session
 from app.Actions.OpenGoogleSheetsConnection import open_google_spreadsheet_connection
 from app.Domain_Actions.GetCurrentDate import get_current_date
@@ -14,33 +12,9 @@ from app.Domain_Actions.CleanUpAutoSheet import clean_up_auto_sheet
 from app.Domain_Actions.CleanUpManualSheet import clean_up_manual_sheet
 
 
-def update_cell_value(state, spreadsheet_name: str, worksheet_name: str, cell_index: str, value: str) -> None:
-    worksheet = state.get(f"worksheet_{spreadsheet_name}{worksheet_name}")
-    worksheet.update_cell_value(cell_index, value)
-    return
-
-
-def open_google_worksheet(state, spreadsheet_name: str, worksheet_name: str) -> None:
-    worksheet = GoogleWorksheetSession(state, GoogleSheetConnection(state), spreadsheet_name, worksheet_name)
-    state.set({f"worksheet_{spreadsheet_name}{worksheet_name}": worksheet})
-    return
-
-
-def get_sheet_cell_value(state, spreadsheet_name: str, worksheet_name: str, index: str) -> str:
-    worksheet = state.get(f"worksheet_{spreadsheet_name}{worksheet_name}")
-    return worksheet.get_cell_value(index)
-
-
-def get_all_sheet_values(state, spreadsheet_name: str, worksheet_name: str):
-    worksheet = state.get(f"worksheet_{spreadsheet_name}{worksheet_name}")
-    worksheet_values = worksheet.get_all_sheet_values()
-    state.set({f"worksheet_{spreadsheet_name}{worksheet_name}_values": worksheet_values})
-    return
-
-
 def main():
     state = State()
-    state.info(__name__, "State set - starting script.")
+    state.info(__name__, "Starting script.")
 
     open_google_spreadsheet_connection(state)
     open_google_worksheet_session(state, state.get("date_spreadsheet"), state.get("date_worksheet"), 'date_worksheet')
@@ -54,21 +28,14 @@ def main():
     get_historical_sheet_values(state)
 
     assemble_daily_food_transfer_data(state)
-
     state.info(__name__, "Sleeping for 60 seconds...")
     time.sleep(60)
-
     transfer_daily_data_to_historical_sheet(state)
-
     state.info(__name__, "Sleeping for 60 seconds...")
     time.sleep(60)
-
-    state.info(__name__, "Blanking auto items...")
+    
     clean_up_auto_sheet(state)
-
-    state.info(__name__, "Blanking manual items...")
     clean_up_manual_sheet(state)
-
     state.info(__name__, "Finished running script.")
     return
 
--- app/tests/test_foodDailyUpdate.py
+++ app/tests/test_foodDailyUpdate.py
@@ -1,22 +1,5 @@
 import mock
-
 from app.foodDailyUpdate import main
-from app.foodDailyUpdate import open_google_worksheet
-from app.foodDailyUpdate import get_sheet_cell_value
-from app.foodDailyUpdate import get_all_sheet_values
-from app.foodDailyUpdate import update_cell_value
-from app.foodDailyUpdate import get_current_date
-from app.foodDailyUpdate import get_manual_sheet_values
-from app.foodDailyUpdate import get_auto_sheet_values
-from app.foodDailyUpdate import assemble_daily_food_transfer_data
-from app.foodDailyUpdate import get_historical_sheet_values
-from app.foodDailyUpdate import transfer_daily_data_to_historical_sheet
-from app.foodDailyUpdate import clean_up_auto_sheet
-from app.foodDailyUpdate import clean_up_manual_sheet
-from app.tests.mock_interfaces.mock_EnvVarProvider import MockEnvVarProvider
-from app.Actions.OpenGoogleWorksheetSession import open_google_worksheet_session
-from app.Actions.OpenGoogleSheetsConnection import open_google_spreadsheet_connection
-
 from app.State import State
 
 
@@ -34,39 +17,3 @@ class TestMain:
         mock_state.return_value = State(env_var_provider=test_env_var_provider)
         main()
         assert True
-
-
-class TestOpenGoogleWorksheet:
-    def test_open_worksheet_is_on_state(self, test_state):
-        open_google_worksheet(test_state, "IntegrationTest", "Sheet1")
-        test_session = test_state.get("worksheet_IntegrationTestSheet1")
-        assert test_session is not None
-
-
-class TestGetSheetCellValue:
-    def test_able_to_retrieve_value(self, test_state):
-        open_google_worksheet(test_state, "IntegrationTest", "Sheet1")
-        test_value = get_sheet_cell_value(test_state, "IntegrationTest", "Sheet1", "A1")
-        assert test_value == "A"
-
-
-class TestGetAllSheetValues:
-    def test_able_to_get_all_values(self, test_state):
-        open_google_worksheet(test_state, "IntegrationTest", "Sheet1")
-        get_all_sheet_values(test_state, "IntegrationTest", "Sheet1")
-        expected_output = [["A", "B", "C", "Test Value", "Date Test"],
-                           ["1", "4", "7", "Test Value 1", "01-Jan-2000"],
-                           ["2", "5", "8", "Test Value 2", ""],
-                           ["3", "6", "9", "Test Value 3", ""]]
-        assert test_state.get("worksheet_IntegrationTestSheet1_values") == expected_output
-
-
-class TestUpdateCellValue:
-    def test_cell_value_updates_correctly(self, test_state):
-        open_google_worksheet(test_state, "IntegrationTest", "PutTest")
-        update_cell_value(test_state, "IntegrationTest", "PutTest", "A1", "")
-        assert get_sheet_cell_value(test_state, "IntegrationTest", "PutTest", "A1") == ""
-        update_cell_value(test_state, "IntegrationTest", "PutTest", "A1", "Test Data")
-        assert get_sheet_cell_value(test_state, "IntegrationTest", "PutTest", "A1") == "Test Data"
-        update_cell_value(test_state, "IntegrationTest", "PutTest", "A1", "")
-        assert get_sheet_cell_value(test_state, "IntegrationTest", "PutTest", "A1") == ""
